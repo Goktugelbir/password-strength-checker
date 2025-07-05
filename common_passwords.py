1332b7a9fca69e9f3a314a0e90d65f8dbec060d3
Göktuğ <146710525+Goktugelbir@users.noreply.github.com>
2025-07-05 17:55:52 +0300

    common_passwords.py

--- common_passwords.py
+++ common_passwords.py
@@ -2,6 +2,6 @@ COMMON_PASSWORDS = [
     "123456", "password", "123456789", "12345678", "12345",
     "1234567", "qwerty", "abc123", "password1", "111111",
     "123123", "admin", "letmein", "welcome", "monkey",
-    "football", "iloveyou", "dragon", "sunshine", "princess"
+    "football", "iloveyou", "dragon", "sunshine", "princess",
     "basketball","1234","654321"
 ]
